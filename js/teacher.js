a306f5b00d05440314a4fd8ae69ee2af6970519f
Jesus Cristancho <jesusmanuelcp@ufps.edu.co>
2022-11-27 21:48:04 -0500

    usuario

---
+++ js/teacher.js
@@ -0,0 +1,64 @@
+const url='http://localhost:8080/users/'
+  function buscarusuario() {
+    var c =document.querySelector("#buscarid")
+    var i =url+c.value
+    consulta(i)
+  }
+
+  function usuarios(){
+    var i='http://localhost:8080/users/'
+    consultaAll(url)
+  }
+
+  function consultapost(){
+    var c =document.querySelector("#imput1")
+    var i = c.value
+    console.log(i);
+      fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({
+              "userName": "pepito",
+              "userCode": 115184222,
+              "identifyCardNumber": 122445218,
+              "email": i+"@ufps.edu.co",
+              "password": "brayan123",
+              "admin": true
+            })
+          })
+      .then(response => response.json())
+      .then(json => console.log(json))
+  }
+
+  function consulta(i){
+    fetch(i)
+      .then(response => response.json())
+      .then(data =>print(data));
+  }
+  function consultaAll(i){
+    fetch(i)
+      .then(response => response.json())
+      .then(data =>printAll(data));
+  }
+
+
+  function print(data) {
+
+    var text = document.querySelector("#teachers")
+    var str =""
+    for(var i in data){
+      str += data[i] + "\n";
+    }
+  text.value=str;
+}
+function printAll(data) {
+
+  var text = document.querySelector("#teachers")
+  var str =""
+  for(var i in data){
+    for(var j in data[i]){
+      str += data[i][j] + "\n";
+  }
+  "\n"
+}
+text.value=str;
+}
